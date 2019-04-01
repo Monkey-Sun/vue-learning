73c5e0a0dead0e4a2d6aa0ef852cea660346839d
MonkeySun <492899051@qq.com>
2019-04-01 12:24:50 +0800

    新增父控件与子控件交互，传值和Vuex的使用

---
+++ src/components/date_cell.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <div class="cell" @click="cellTaped">
+      <p>标题</p>
+      <p>{{ item }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props:["item", "index"],
+  methods: {
+    cellTaped() {
+      this.$emit("cellClickAt", `你点我干嘛?${this.index}`);
+    }
+  }
+};
+</script>
+
+<style lang="">
+.cell {
+  height: 50px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+}
+</style>
--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -2,21 +2,29 @@
   <div id="home">
     <p>i'm home</p>
     <div v-for="(item, index) in items" :key="index">
-      <div class="cell">
-        <p>标题</p>
-        <p>日期</p>
-      </div>
+      <date_cell @cellClickAt="subcomTaped" :item = "item" :index = "index"/>
     </div>
   </div>
 </template>
 
 <script>
+import date_cell from "@/components/date_cell";
+
+import Store from "@/store";
+
 export default {
-  name:'Home',
+  name: "Home",
+  components: { date_cell },
   data() {
     return {
       items: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
     };
+  },
+  methods: {
+    subcomTaped(e) {
+      alert(e);
+      Store.commit('increment');
+    }
   }
 };
 </script>
@@ -27,13 +35,4 @@ export default {
   flex-direction: column;
   align-items: stretch;
 }
-
-.cell{
-    height: 50px;
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-    align-items: center;
-}
-
 </style>
